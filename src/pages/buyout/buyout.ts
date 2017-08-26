a2c4b12e602de5ccfc6dfde7b49bb9561a79ce41
Pranav Jagtap <pranav.jagtap@digiperfect.com>
2017-08-26 22:03:09 +0530

    Removed commented code.

--- src/pages/buyout/buyout.ts
+++ src/pages/buyout/buyout.ts
@@ -1,13 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the BuyoutPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-buyout',
@@ -26,8 +19,5 @@ export class BuyoutPage implements OnInit {
   onGoToHomePage() {
     this.navCtrl.popToRoot();
   }
-  // ionViewDidLoad() {
-  //   console.log('ionViewDidLoad BuyoutPage');
-  // }
 
 }
